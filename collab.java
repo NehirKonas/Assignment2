006ae8747641e80587fbde561a95206053d31137
Selen Güneri <selenxguneri2@gmail.com>
2024-02-07 16:22:22 +0300

    max and min methods

--- collab.java
+++ collab.java
@@ -15,5 +15,41 @@ class collab {
             System.out.println("Choose ");
             
         } while (!exiting);
+
+
+    }
+
+    //method for finding min
+    public static int findMin(int [] array)
+    {
+        int temp = 100;
+        int min;
+        for (int i = 0; i < array.length; i++)
+        {
+            int num = array[i];
+            if (num <= temp)
+            {
+                temp = num;
+            }
+            min = num;
+        }
+        return min;
+    }
+
+    //method for finding max
+    public static int findMax(int[] array)
+    {
+        int temp = 0;
+        int max;
+        for (int i = 0; i < array.length; i++)
+        {
+            int num = array[i];
+            if (num >= temp)
+            {
+                temp = num;
+            }
+            max = num;
+        }
+        return max;
     }
 }
