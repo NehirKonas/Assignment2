6f1ef284392d9599f113aa07ff84c017331626a1
Nehir Konaş <nehirkonas@gmail.com>
2024-02-07 15:55:27 +0300

    main oluşum

--- collab.java
+++ collab.java
@@ -1,1 +1,19 @@
+package Assignment2;
 
+import java.util.Random;
+
+class collab {
+    public static void main(String[] args) {
+        Random ran = new Random();
+        boolean exiting = false;
+        int arraySize = 10;
+        int[] arr = new int[arraySize];
+        for(int i = 0 ; i < arraySize; i++){
+            arr[i] = ran.nextInt(101);
+        }
+        do {
+            System.out.println("Choose ");
+            
+        } while (!exiting);
+    }
+}
