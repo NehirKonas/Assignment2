402462c0a129dd085f1d183a38bca3285e06fa7c
Nehir Konaş <nehirkonas@gmail.com>
2024-02-07 16:16:08 +0300

    Menu oluşum

--- collab.java
+++ collab.java
@@ -1,19 +1,44 @@
 package Assignment2;
 
 import java.util.Random;
+import java.util.Scanner;
 
 class collab {
+    public static int arraySize = 10;
     public static void main(String[] args) {
         Random ran = new Random();
+        Scanner scanner = new Scanner(System.in);
+        int userInput;
         boolean exiting = false;
-        int arraySize = 10;
+        
         int[] arr = new int[arraySize];
         for(int i = 0 ; i < arraySize; i++){
             arr[i] = ran.nextInt(101);
         }
+
         do {
-            System.out.println("Choose ");
-            
+            System.out.println("Choose the operation: \n1.Find min \n2.Find max \n3.Find average \n4.Find sum of element depending on odd indexes.\n5.Find sum of element depending on even indexes.\n6.Exit.");
+            userInput = scanner.nextInt();
+            if(userInput==1){
+                System.out.println("Min value of array: " + findMin(arr));
+            }
+            else if(userInput==2){
+                System.out.println("Max value of array: " + findMiax(arr));
+            }
+            else if(userInput==3){
+                System.out.println("Min value of array: " + findSum(arr));
+            }
+            else if(userInput==4){
+                System.out.println("Min value of array: " + sumOddIndex(arr));
+            }
+            else if(userInput==5){
+                System.out.println("Min value of array: " + sumEvenIndex(arr));
+            }
+            else if(userInput==6){
+                System.out.println("Exiting.");
+                exiting = true;
+                break;
+            }
         } while (!exiting);
     }
 }
